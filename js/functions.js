0d9f0ef3f1467cb0f17af170d97039a0033c646e
yauris <yauriscarrillo@gmail.com>
2021-01-13 16:12:43 -0500

    subiendo nuevo contenido

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+/* btn click menu */
+let btnMenuHamburguess = document.getElementById('btn-menu');
+btnMenuHamburguess.addEventListener("click", () => {
+  document.getElementById('menu').classList.toggle('d-none');
+  document.getElementById('menu-desing').classList.toggle('d-none');
+ 
+});
+/* ================================ */
